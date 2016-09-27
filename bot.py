7eed5ee6dacc18b5d41c16e3db6a115c412dc314
ashaposhnikova <polosate@gmail.com>
2016-09-27 18:15:54 +0300

    Telegram bot

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(bot, update):
+	print("Вызван /start")
+	bot.sendMessage(update.message.chat_id, text='Привет! Я бот, который помогает учить Python!')
+
+def talk_to_me(bot, update):
+	print('Пришло сообщение: %s' % update.message.text)
+	bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+def run_bot():
+	updater = Updater("295434127:AAE60azMR4b0nashTLGai9DR9GNrjA8UQu0")
+
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
+
+	updater.start_polling()
+	updater.idle()
+
+if __name__ == "__main__":
+	run_bot()
+
