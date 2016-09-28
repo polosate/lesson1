f2936c2e0fbe881091c4adce34f53ee7ccf0e98e
ashaposhnikova <polosate@gmail.com>
2016-09-28 16:50:30 +0300

    add handling command /time

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from answers import answers, get_answers
+import datetime
 
 def start(bot, update):
 	print("Вызван /start")
@@ -11,11 +12,15 @@ def talk_to_me(bot, update):
 	print('Пришло сообщение: %s' % update.message.text)
 	bot.sendMessage(update.message.chat_id, text=get_answers(update.message.text, answers))
 
+def time(bot, update):
+	bot.sendMessage(update.message.chat_id, text=datetime.datetime.now().strftime("%A, %d.%m.%y, %H:%M:%S"))
+
 def run_bot():
 	updater = Updater("295434127:AAE60azMR4b0nashTLGai9DR9GNrjA8UQu0")
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(CommandHandler("time", time))
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
 	updater.start_polling()
